ea830ec4e96592203a8e07388d85b0b8ff9adf6a
Abhishek Jaiswal <abhigrmr@gmail.com>
2023-09-25 22:25:26 +0530

    added the postgress db into env files

--- config/database.js
+++ config/database.js
@@ -1,13 +1,33 @@
-const { Sequelize } = require('sequelize');
+const { Pool } = require('pg');
+require('dotenv').config();
 
-const sequelize = new Sequelize({
-    dialect: 'postgres',
-    username: 'your_username',
-    password: 'your_password',
-    database: 'your_database',
-    host: 'localhost', // Your database host
-    port: 5432,        // Default PostgreSQL port
-    logging: false     // Disable logging SQL queries (optional)
-});
+// const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     username: 'your_username',
+//     password: 'your_password',
+//     database: 'your_database',
+//     host: 'localhost', // Your database host
+//     port: 5432,        // Default PostgreSQL port
+//     logging: false     // Disable logging SQL queries (optional)
+// });
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
+try {
+    pool.connect((err) => {
+        if (err) {
+            console.log('PostgreSQL URL:', process.env.POSTGRES_URL);
+            console.log("Error connecting to database", err);
+        }
+        else {
+            //to get information about the vercel database
+            //console.log(pool)
+            console.log("Database connection established");
+        }
+    })
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
 
-module.exports = sequelize;
+
+module.exports = pool;
--- models/user.js
+++ models/user.js
@@ -1,16 +1,48 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+// // const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/database');
 
-const User = sequelize.define('User', {
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+// const User = sequelize.define('User', {
+//     username: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     email: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true
+//     }
+// });
+
+// module.exports = User;
+
+const pool = require('../config/database');
+
+// Define the User model using pg-promise
+class User {
+    static async findAll() {
+        try {
+            const query = 'SELECT * FROM users'; // Replace 'users' with your actual table name
+            const { rows } = await pool.query(query);
+            return rows;
+        } catch (error) {
+            console.error('Error querying users:', error);
+            throw error;
+        }
     }
-});
+
+    static async create(username, email) {
+        try {
+            const query = 'INSERT INTO users (username, email) VALUES ($1, $2) RETURNING *';
+            const values = [username, email];
+            const { rows } = await pool.query(query, values);
+            return rows[0];
+        } catch (error) {
+            console.error('Error creating user:', error);
+            throw error;
+        }
+    }
+
+    // Add more model methods as needed
+}
 
 module.exports = User;
