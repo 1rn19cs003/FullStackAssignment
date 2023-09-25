f7ac968bb8fc1ccf675f970c607e37dffe025010
Abhishek Jaiswal <abhigrmr@gmail.com>
2023-09-25 21:54:00 +0530

    added the error hasndling issue has been faced during depoyment of server

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,11 @@ const User = require('../models/User');
 
 exports.getUsers = async (req, res) => {
     try {
-        const users = await User.findAll();
-        res.json(users);
+        // const users = await User.findAll();
+        // res.json(users);
+        res.status(200).send({message:"Your backend is working fine "});
     } catch (error) {
         console.error(error);
-        
         res.status(500).send({message:'Server Error'});
     }
 };
